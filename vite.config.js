6af0fc3d3d23f679f7b846e42f938c2c6c2a503a
Krish_Script <mandalkrishnendu2006@outlook.com>
2026-06-07 21:35:17 +0530

    Deployment

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/Portfolio/' : '/',
+  base: mode === 'production' ? '/https://github.com/gpriyam6002-dev/Portfolio/' : '/',
   plugins: [react()],
 }))
